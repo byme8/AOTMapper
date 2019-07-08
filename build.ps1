5732d4e3b0bec32e36067dc6717c04deb5379aa8
Stanislav Silin <silin.stanislav@outlook.com>
2019-07-08 16:45:19 +0300

    Added build script

---
+++ build.ps1
@@ -0,0 +1,17 @@
+using namespace System.IO;
+try {
+    $nugetToken = [File]::ReadAllText("nugetToken");
+}
+catch {
+    echo "Nuget token is missing"
+    exit
+}
+
+rm -r ./nugets
+dotnet pack -o ./../../nugets ./AOTMapper/AOTMapper/AOTMapper.csproj
+dotnet pack -o ./../nugets ./AOTMapper.Core/AOTMapper.Core.csproj
+
+$files = [Directory]::GetFiles("./nugets")
+foreach ($file in $files) {
+    dotnet nuget push $file -k $nugetToken -s https://api.nuget.org/v3/index.json
+}

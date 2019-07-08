e494e0899de7373805f8f48c5f5b1e9c1dd00195
Stanislav Silin <silin.stanislav@outlook.com>
2019-07-08 16:04:27 +0300

    Started work on benchmark

---
+++ AOTMapper.Benchmark/AOTMapperVSAutoMapper.cs
@@ -0,0 +1,39 @@
+﻿using AOTMapper.Benchmark.Data;
+using AutoMapper;
+
+namespace AOTMapper.Benchmark
+{
+    public class AOTMapperVSAutoMapper
+    {
+        private IMapper mapper;
+
+        public UserEntity UserEntity { get; }
+        public User User { get; }
+
+        public AOTMapperVSAutoMapper()
+        {
+            var configuration = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<User, UserEntity>();
+                cfg.CreateMap<UserEntity, User>()
+                    .AfterMap((entity, user) => user.Name = $"{entity.FirstName} {entity.LastName}");
+            });
+
+            configuration.AssertConfigurationIsValid();
+            this.mapper = configuration.CreateMapper();
+
+            this.UserEntity = new UserEntity();
+            this.User = this.mapper.Map<User>(this.UserEntity);
+        }
+
+        public void AutoMapperToUserEntity()
+        {
+            var userEntity = this.mapper.Map<UserEntity>(this.User);
+        }
+
+        public void AutoMapperToUser()
+        {
+            var userEntity = this.mapper.Map<User>(this.User);
+        }
+    }
+}
---
+++ AOTMapper.Benchmark/Data/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOTMapper.Benchmark.Data
+{
+    public class Entity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+    }
+
+    public class UserEntity : Entity
+    {
+        public string FirstName { get; set; } = Guid.NewGuid().ToString();
+        public string LastName { get; set; } = Guid.NewGuid().ToString();
+    }
+
+    public class User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string Name { get; set; }
+    }
+}

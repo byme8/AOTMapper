b61619cfd5e19bcb236d4602cea33f93e0ba4c68
Stanislav Silin <silin.stanislav@outlook.com>
2025-08-13 16:26:20 +0300

    Improve nuget package.

--- build.ps1
+++ build.ps1
@@ -1,21 +1,7 @@
 using namespace System.IO;
-try {
-    $nugetToken = [File]::ReadAllText("./nugetToken");
-}
-catch {
-    echo "Nuget token is missing"
-    exit
-}
 
 rm -r ./nugets
 dotnet build -c release ./AOTMapper/AOTMapper/AOTMapper.csproj
 dotnet build -c release ./AOTMapper.Core/AOTMapper.Core.csproj
 dotnet pack  -c release -o ./nugets ./AOTMapper/AOTMapper/AOTMapper.csproj
 dotnet pack  -c release -o ./nugets ./AOTMapper.Core/AOTMapper.Core.csproj
-
-if (($args.Count -gt 0) -and ($args.Contains("--publish"))) {
-    $files = [Directory]::GetFiles("./nugets")
-    foreach ($file in $files) {
-        dotnet nuget push $file -k $nugetToken -s https://api.nuget.org/v3/index.json
-    }
-}
